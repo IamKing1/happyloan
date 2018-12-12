14adaeb69df494899eb1c08f63b953b6553b1928
xiaonixn00 <18339453177@163.com>
2018-12-12 10:12:51 +0800

    111

--- src/main/java/com/group8/controller/test.java
+++ src/main/java/com/group8/controller/test.java
@@ -1,10 +1,19 @@
 package com.group8.controller;
 
 
+import com.group8.util.FTPfile;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * className:testList
@@ -27,19 +36,42 @@ public class test {
 		System.out.println("构建完成1111");
 		return "user";
 	}
-	/**
-	 * 图片显示方法
-	 * @param fileName
-	 * @return
-	 */
-	/*@RequestMapping("/showFtp")
-	public ResponseEntity showFtp(String fileName){
-		try {
-			// 由于是读取本机的文件，file是一定要加上的， path是在application配置文件中的路径
-			Resource resource = resourceLoader.getResource("ftp://p2p:aaa@172.16.22.62/"+fileName);
-			return ResponseEntity.ok(resourceLoader.getResource("ftp://p2p:aaa@172.16.22.62/"+fileName));
-		} catch (Exception e) {
-			return ResponseEntity.notFound().build();
+
+	@RequestMapping("/user/addImg")
+	@ResponseBody
+	public String addImg(@RequestBody MultipartFile file, HttpServletRequest request, HttpServletResponse response)throws Exception{
+		System.out.println("上传图片是否为空："+file.isEmpty());
+		if(file != null){
+			String path =null;// 文件路径
+			String imgType=null;//图片类型
+			String  fileName = file.getOriginalFilename();// 原文件名称
+			// 判断图片类型
+			imgType=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
+			if(imgType!=null){
+//				if("GIF".equals(imgType.toUpperCase()) || "PNG".equals(imgType.toUpperCase()) || "JPG".equals(imgType.toUpperCase())){
+					// 项目在容器中实际发布运行的根路径
+					/* String realPath = request.getSession().getServletContext().getRealPath("/");*/
+					String realPath = "E:\\upload\\";
+					// 自定义的文件名称
+					String trueFileName=String.valueOf(System.currentTimeMillis())+fileName;
+					// 设置图片存放的路径
+					path=realPath+trueFileName;
+					System.out.println("图片的存放路径为"+path);
+					// 转存文件到指定路径
+					file.transferTo(new File(path)); // 转存而不是写出
+					System.out.println("文件成功上传到指定目录下");
+				/*}else{
+					System.out.println("请上传GIF、PNG或者JPG格式的文件");
+				}*/
+			}else{
+				System.out.println("文件类型为空");
+			}
+			return path;
+		}else{
+			System.out.println("没有找到相对应的文件");
 		}
-	}*/
+		System.out.println("文件上传的原名称为:"+file.getOriginalFilename());
+
+		return "";
+	}
 }
